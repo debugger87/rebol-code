7b9a487a8b6d87ad47557543fd06354f9f542909
debugger87 <yangchaozhong.2009@gmail.com>
2013-09-21 01:51:05 +0800

    rot13

---
+++ rot13.r
@@ -0,0 +1,13 @@
+REBOL [
+    title: "exercise programs I"
+    author: "debugger87"
+    date: 20-Sep-2013
+    version: 1.0.0
+]
+str1: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+str2: "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+rot13: func [ char ] [
+
+    either found? find str1 char [ return pick str2 index? find/case str1 char ][ return
+    char ]
+]
