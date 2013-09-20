9b932ae2a59ab1f80c26eed65cbe7e83c77d1e6c
debugger87 <yangchaozhong.2009@gmail.com>
2013-09-20 23:38:28 +0800

    2.r

---
+++ 2.r
@@ -0,0 +1,14 @@
+REBOL [
+    title: "exercise programs I"
+    author: "debugger87"
+    date: 20-Sep-2013
+    version: 1.0.0
+]
+data: [87 45 67]
+max-data: first data
+foreach item data [
+    if > item max-data [
+        max-data: item
+    ]
+]
+write %abc-max.r max-data
